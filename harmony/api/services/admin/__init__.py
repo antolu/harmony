97c2cf90788a36a8c3568c3b43147ae0f8205944
Anton Lu <anton96lu@gmail.com>
2026-06-22 20:39:53 +0200

    HRM-170 feat(api): Add OllamaHostService and LLMApiKeyService with conditional delete semantics

--- harmony/api/services/admin/__init__.py
+++ harmony/api/services/admin/__init__.py
@@ -5,6 +5,7 @@ from harmony.api.services.admin._config_store import ConfigStore, config_store
 from harmony.api.services.admin._crawl_config import CrawlConfigService
 from harmony.api.services.admin._indexer_config import IndexerConfigService
 from harmony.api.services.admin._job_manager import JobManager
+from harmony.api.services.admin._llm_api_keys import LLMApiKeyService
 from harmony.api.services.admin._log_streamer import LogStreamer
 from harmony.api.services.admin._model_policy import ModelPolicyStore
 from harmony.api.services.admin._model_registry import ModelRegistryService
@@ -13,6 +14,7 @@ from harmony.api.services.admin._model_settings import (
     ModelSettingsStore,
     Provider,
 )
+from harmony.api.services.admin._ollama_hosts import DeleteResult, OllamaHostService
 from harmony.api.services.admin._scheduler import ScheduleService
 from harmony.api.services.admin._service_config import ServiceConfigStore
 from harmony.api.services.admin._webhook_service import WebhookService
@@ -21,13 +23,16 @@ __all__ = [
     "AuditLogService",
     "ConfigStore",
     "CrawlConfigService",
+    "DeleteResult",
     "IndexerConfigService",
     "JobManager",
+    "LLMApiKeyService",
     "LogStreamer",
     "ModelPolicyStore",
     "ModelRegistryService",
     "ModelSettings",
     "ModelSettingsStore",
+    "OllamaHostService",
     "Provider",
     "ScheduleService",
     "ServiceConfigStore",
